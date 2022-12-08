368f3441318e93a6b55b8c588597ca620741d186
hung yu <yuehyu1028@gmail.com>
2022-12-08 12:57:37 +0800

    Add

---
+++ app.js
@@ -0,0 +1,40 @@
+const num1=Math.ceil(Math.random()*10);
+const num2=Math.ceil(Math.random()*10);
+// console.log(num1);
+
+const questionEl=document.getElementById("question");
+
+const inputEl=document.getElementById("input");
+
+const formEl=document.getElementById("form");
+
+questionEl.innerText=`What is ${num1} multiply by ${num2}?`;
+
+const correctAns=num1*num2;
+
+const scoreEl=document.getElementById("score");
+let score=JSON.parse(localStorage.getItem("score"));
+
+if(!score){
+    score=0;
+}
+
+scoreEl.innerText=`score:${score}`;
+
+formEl.addEventListener("submit",()=>{
+    const userAns=+inputEl.value
+    // console.log(userAns,typeof userAns)
+    if(userAns===correctAns){
+        score++;
+        updateLocalStorage();
+        // console.log(score);
+    }else{
+        score--
+        updateLocalStorage();
+        // console.log(score);
+    }
+});
+
+function updateLocalStorage(){
+    localStorage.setItem("score",JSON.stringify(score));
+}
